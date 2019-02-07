8ac64a19aecdadf05aa1908f8c2e7ec1ea3ba198
DESKTOP-4OOTLQN\kjhmd <kjhmd9726@gmail.com>
2019-02-07 18:29:41 +0900

    [Fix] Fix router component and add private router.

--- src/router.js
+++ src/router.js
@@ -1,24 +1,56 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
+import { connect } from 'react-redux';
 
 // Import pages.
 import SignIn from './containers/SignIn';
 import Main from './containers/Main';
-import Fzf from './containers/Pages/404';
 
-const AppRouter = () => {
+import ErrorPage404 from './containers/Pages/404';
+
+// Router before login.
+const AppRouter = ({ isSignIn }) => {
   return (
     <Router>
-      <div>
         <Switch>
-          <Route exact path="/" component={SignIn}/>
-          <Route exact path="/signin" component={SignIn}/>
-          <Route exact path="/main" component={Main}/>
-          <Route component={Fzf}/>
+          <Route exact path="/" component={SignIn} />
+          <Route exact path="/signin" component={SignIn} />
+          <PrivateRouterComponent exact isSignIn={isSignIn} path="/main" component={PrivateRouter} />
+          <Route component={ErrorPage404}/>
         </Switch>
-      </div>
     </Router>
   );
 };
 
-export default AppRouter
+// Private Router component
+const PrivateRouterComponent = ({ component: Component, isSignIn, ...rest }) => {
+  return(
+    <Route {...rest} render={props => (
+        // Check is signin.
+        isSignIn === true
+          ? <Component {...props} />
+          : <Redirect to={{
+              pathname: '/signin',
+              state: { from: props.location }
+            }} />
+    )}/>
+  );
+};
+
+// Router after login.
+const PrivateRouter = () => {
+  return(
+    <Switch>
+      <Route exact path="/main" component={Main} />
+    </Switch>
+  );
+};
+
+// Map state to props.
+const _mapStateToProps = (state) => {
+  const auth = state.Auth;
+  return { isSignIn: auth.isSignIn };
+};
+
+// Connect state to SignIn props.
+export default connect(_mapStateToProps)(AppRouter);
