52858a7b791780fc8c29cf416a65e5eff16422af
DESKTOP-4OOTLQN\kjhmd <kjhmd9726@gmail.com>
2019-03-06 23:01:27 +0900

    [Fix] Fix header component. Done.

--- src/components/DrawerList/DrawerList.js
+++ src/components/DrawerList/DrawerList.js
@@ -5,6 +5,8 @@ import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
+import Divider from '@material-ui/core/Divider';
+import { withStyles } from '@material-ui/core/styles';
 
 // Import material ui icons.
 import Home from '@material-ui/icons/Home';
@@ -37,10 +39,20 @@ const drawerList = [{
   link: '/admin'
 }];
 
-const DrawerList = () => {
+const drawerListStyles = theme => ({
+  drawerWrapper: {
+    width: 240
+  },
+  toolbar: theme.mixins.toolbar,
+});
+
+const DrawerList = (props) => {
+  const { classes } = props;
+
   return(
-    <div className="drawer">
-      <div className="drawer__toobar" />
+    <div className={`${classes.drawerWrapper} drawer-wrapper`}>
+      <div className={`${classes.toolbar} drawer-wrapper__empty-space`} />
+      <Divider />
       <List>
         {drawerList.map(({ iconComponent, text, link }, index) => (
           <ListItem component={Link} to={link} key={index} button>
@@ -53,4 +65,4 @@ const DrawerList = () => {
   )
 };
 
-export default DrawerList;
+export default withStyles(drawerListStyles)(DrawerList);
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -8,6 +8,7 @@ import Hidden from '@material-ui/core/Hidden';
 import IconButton from '@material-ui/core/IconButton';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
+import { withStyles } from '@material-ui/core/styles';
 
 // Import material ui icons.
 import MenuIcon from '@material-ui/icons/Menu';
@@ -15,24 +16,58 @@ import MenuIcon from '@material-ui/icons/Menu';
 import DrawerList from './../DrawerList/DrawerList';
 import './Header.css';
 
+const drawerWidth = 240;
+
+const appBarStyles = theme => ({
+  root: {
+    display: 'flex',
+  },
+  drawer: {
+    [theme.breakpoints.up('lg')]: {
+      width: drawerWidth,
+      flexShrink: 0,
+    },
+  },
+  appBar: {
+    marginLeft: drawerWidth,
+    [theme.breakpoints.up('lg')]: {
+      width: `calc(100% - ${drawerWidth}px)`,
+    },
+  },
+  menuButton: {
+    marginRight: 20,
+    [theme.breakpoints.up('lg')]: {
+      display: 'none',
+    },
+  },
+  toolbar: theme.mixins.toolbar,
+  drawerPaper: {
+    width: drawerWidth,
+  },
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing.unit * 3
+  },
+});
+
 const Header = (props) => {
-  const { container, children } = props;
+  const { container, children, classes } = props;
   const [drawerOpen, setDrawerOpen] = React.useState(false);
 
   const handleDrawerToggle = () => setDrawerOpen(!drawerOpen);
 
   return (
-    <div className="app-bar-wrapper">
+    <div className={`${classes.root} app-bar-wrapper`}>
       <CssBaseline />
-      <AppBar position="fixed" className="app-bar-wrapper__app-bar">
-        <Toolbar className="app-bar-wrapper__app-bar__tool-bar">
+      <AppBar position="fixed" className={`${classes.appBar} app-bar-wrapper__app-bar`}>
+        <Toolbar className={`app-bar-wrapper__app-bar__tool-bar`}>
           <IconButton
             color="inherit"
             aria-label="Open drawer"
             onClick={handleDrawerToggle}
-            className="app-bar-wrapper__app-bar__tool-bar__icon-button"
+            className={`${classes.menuButton} app-bar-wrapper__app-bar__tool-bar__icon-button`}
           >
-            <MenuIcon className="app-bar-wrapper__app-bar__tool-bar__icon-button__icon"/>
+            <MenuIcon className={`app-bar-wrapper__app-bar__tool-bar__icon-button__icon`} />
           </IconButton>
           <Typography
             variant="h6"
@@ -43,10 +78,10 @@ const Header = (props) => {
           </Typography>
         </Toolbar>
       </AppBar>
-      <nav className="app-bar-wrapper__drawer-wrapper">
+      <nav className={`${classes.drawer} app-bar-wrapper__drawer-wrapper`}>
         {/* Width is smaller than 1280px */}
         <Hidden
-          className="app-bar-wrapper__drawer-wrapper__hidden--md-up"
+          className={`app-bar-wrapper__drawer-wrapper__hidden--md-up`}
           implementation="css"
           mdUp
         >
@@ -54,9 +89,10 @@ const Header = (props) => {
             container={container}
             variant="temporary"
             anchor="left"
+            // anchor={theme.direction === 'rtl' ? 'right' : 'left'}
             open={drawerOpen}
             onClose={handleDrawerToggle}
-            className="app-bar-wrapper__drawer-wrapper__hidden--md-up__drawer drawer"
+            className={`${classes.drawerPaper} app-bar-wrapper__drawer-wrapper__hidden--md-up__drawer drawer-wrapper`}
             // Better open performance on mobile.
             ModalProps={{ keepMounted: true }}
           >
@@ -65,12 +101,12 @@ const Header = (props) => {
         </Hidden>
         {/* Width is bigger then 1280px */}
         <Hidden
-          className="app-bar-wrapper__drawer-wrapper__hidden--md-down"
+          className={`app-bar-wrapper__drawer-wrapper__hidden--md-down`}
           implementation="css"
           mdDown
         >
           <Drawer
-            className="app-bar-wrapper__drawer-wrapper__hidden--md-down__drawer drawer"
+            className={`${classes.drawerPaper} app-bar-wrapper__drawer-wrapper__hidden--md-down__drawer drawer-wrapper`}
             variant="permanent"
             open
           >
@@ -78,12 +114,12 @@ const Header = (props) => {
           </Drawer>
         </Hidden>
       </nav>
-      <main className="app-bar-wrapper__main">
-        {/* <div className="app-bar-wrapper__main__tool-bar" /> */}
+      <main className={`${classes.content} app-bar-wrapper__main`}>
+        <div className={`${classes.toolbar} app-bar-wrapper__main__tool-bar`} />
         {children}
       </main>
     </div>
   );
 }
 
-export default Header;
+export default withStyles(appBarStyles, {widthTheme: true})(Header);
