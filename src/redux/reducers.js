837de6b77e29a93605030622ebddbbff3d8d86ec
DESKTOP-4OOTLQN\kjhmd <kjhmd9726@gmail.com>
2019-05-13 22:13:31 +0900

    [Fix] Fix components.

--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -1,18 +1,4 @@
-<<<<<<< HEAD
 import auth from './auth/reducer';
-import ProjectMain from './project/projectMain/reducer';
-import ProjectView from './project/projectView/reducer';
-import ProjectManipulation from './project/projectManipulation/reducer';
-
-// Combine all reducers.
-export default {
-  auth,
-  ProjectMain,
-  ProjectView,
-  ProjectManipulation
-};
-=======
-import Auth from './auth/reducer';
 import ProjectMain from './project/projectMain/reducer';
 import ProjectView from './project/projectView/reducer';
 import ProjectManipulation from './project/projectManipulation/reducer';
@@ -20,6 +6,9 @@ import IssueMain from './project/issueMain/reducer';
 
 // Combine all reducers.
 export default {
-  Auth, ProjectMain, ProjectView, ProjectManipulation, IssueMain,
+  auth,
+  ProjectMain,
+  ProjectView,
+  ProjectManipulation,
+  IssueMain,
 };
->>>>>>> origin/Kwoo_dev
--- src/redux/sagas.js
+++ src/redux/sagas.js
@@ -2,12 +2,8 @@ import { all } from 'redux-saga/effects';
 import authSaga from './auth/saga';
 import projectMainSaga from './project/projectMain/saga';
 import projectViewSaga from './project/projectView/saga';
-<<<<<<< HEAD
-import projectManipulation from './project/projectManipulation/saga';
-=======
 import projectManipulationSaga from './project/projectManipulation/saga';
 import issueMainSaga from './project/issueMain/saga';
->>>>>>> origin/Kwoo_dev
 
 // Combine all sagas.
 export default function* rootSaga(getState) {
