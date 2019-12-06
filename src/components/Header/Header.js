af4d6f13d06eb4ba76bd7870872c6ef4f165ed31
KangJi <kjhmd9726@gmail.com>
2019-12-07 01:34:32 +0900

    [Refactoring] Refactoring Header component. Use makeStyles function and remove useless props.

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import PropTypes from "prop-types";
-
 import AppBar from "@material-ui/core/AppBar";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import Drawer from "@material-ui/core/Drawer";
@@ -10,53 +9,60 @@ import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
 import Menu from "@material-ui/core/Menu";
 import MenuItem from "@material-ui/core/MenuItem";
-import { withStyles } from "@material-ui/core/styles";
-
+import { makeStyles } from "@material-ui/core/styles";
 import MenuIcon from "@material-ui/icons/Menu";
 import AccountCircle from "@material-ui/icons/AccountCircle";
-
 import DrawerList from "../DrawerList/DrawerList";
-// import './Header.css';
 
 const drawerWidth = 240;
 
-const appBarStyles = theme => ({
-  root: {
+const appBarStyles = makeStyles(theme => ({
+  rootClassName: {
     display: "flex"
   },
-  drawer: {
+  drawerClassName: {
     [theme.breakpoints.up("lg")]: {
       width: drawerWidth,
       flexShrink: 0
     }
   },
-  appBar: {
+  appBarClassName: {
     marginLeft: drawerWidth,
     [theme.breakpoints.up("lg")]: {
       width: `calc(100% - ${drawerWidth}px)`
     }
   },
-  menuButton: {
+  menuButtonClassName: {
     marginRight: 20,
     [theme.breakpoints.up("lg")]: {
       display: "none"
     }
   },
-  toolbar: theme.mixins.toolbar,
-  drawerPaper: {
+  toolbarClassName: theme.mixins.toolbar,
+  drawerPaperClassName: {
     width: drawerWidth
   },
-  content: {
+  contentClassName: {
     flexGrow: 1,
     padding: theme.spacing(3)
   },
-  title: {
+  titleClassName: {
     flexGrow: 1
   }
-});
+}));
 
 const Header = props => {
-  const { container, children, classes } = props;
+  const { children } = props;
+  const {
+    rootClassName,
+    drawerClassName,
+    appBarClassName,
+    menuButtonClassName,
+    toolbarClassName,
+    drawerPaperClassName,
+    contentClassName,
+    titleClassName
+  } = appBarStyles();
   const [drawerOpen, setDrawerOpen] = useState(false);
   const [anchorEl, setAnchorEl] = useState(null);
   const accountCircleMenuOpen = Boolean(anchorEl);
@@ -68,25 +74,25 @@ const Header = props => {
   const handleAccountCircleMenuClose = () => setAnchorEl(null);
 
   return (
-    <div className={`${classes.root} app-bar-wrapper`}>
+    <div className={`${rootClassName} app-bar-wrapper`}>
       <CssBaseline />
       <AppBar
         position="fixed"
-        className={`${classes.appBar} app-bar-wrapper__app-bar`}
+        className={`${appBarClassName} app-bar-wrapper__app-bar`}
       >
         <Toolbar className="app-bar-wrapper__app-bar__tool-bar">
           <IconButton
             color="inherit"
             aria-label="Open drawer"
             onClick={handleDrawerToggleOpen}
-            className={`${classes.menuButton} app-bar-wrapper__app-bar__tool-bar__icon-button`}
+            className={`${menuButtonClassName} app-bar-wrapper__app-bar__tool-bar__icon-button`}
           >
             <MenuIcon className="app-bar-wrapper__app-bar__tool-bar__icon-button__icon" />
           </IconButton>
           <Typography
             variant="h6"
             color="inherit"
-            className={`${classes.title} app-bar-wrapper__app-bar__tool-bar__title`}
+            className={`${titleClassName} app-bar-wrapper__app-bar__tool-bar__title`}
             noWrap
           >
             TCP WEB PROJECT
@@ -121,7 +127,7 @@ const Header = props => {
           </div>
         </Toolbar>
       </AppBar>
-      <nav className={`${classes.drawer} app-bar-wrapper__drawer-wrapper`}>
+      <nav className={`${drawerClassName} app-bar-wrapper__drawer-wrapper`}>
         {/* Width is smaller than 1280px */}
         <Hidden
           className="app-bar-wrapper__drawer-wrapper__hidden--md-up"
@@ -129,13 +135,12 @@ const Header = props => {
           mdUp
         >
           <Drawer
-            container={container}
             variant="temporary"
             anchor="left"
             open={drawerOpen}
             onClose={handleDrawerToggleOpen}
             onClick={handleDrawerToggleClose}
-            className={`${classes.drawerPaper} app-bar-wrapper__drawer-wrapper__hidden--md-up__drawer drawer-wrapper`}
+            className={`${drawerPaperClassName} app-bar-wrapper__drawer-wrapper__hidden--md-up__drawer drawer-wrapper`}
             // Better open performance on mobile.
             ModalProps={{ keepMounted: true }}
           >
@@ -149,7 +154,7 @@ const Header = props => {
           mdDown
         >
           <Drawer
-            className={`${classes.drawerPaper} app-bar-wrapper__drawer-wrapper__hidden--md-down__drawer drawer-wrapper`}
+            className={`${drawerPaperClassName} app-bar-wrapper__drawer-wrapper__hidden--md-down__drawer drawer-wrapper`}
             variant="permanent"
             open
           >
@@ -157,8 +162,10 @@ const Header = props => {
           </Drawer>
         </Hidden>
       </nav>
-      <main className={`${classes.content} app-bar-wrapper__main`}>
-        <div className={`${classes.toolbar} app-bar-wrapper__main__tool-bar`} />
+      <main className={`${contentClassName} app-bar-wrapper__main`}>
+        <div
+          className={`${toolbarClassName} app-bar-wrapper__main__tool-bar`}
+        />
         {children}
       </main>
     </div>
@@ -169,4 +176,4 @@ Header.propTypes = {
   children: PropTypes.oneOfType([PropTypes.element, PropTypes.array]).isRequired
 };
 
-export default withStyles(appBarStyles, { widthTheme: true })(Header);
+export default Header;
