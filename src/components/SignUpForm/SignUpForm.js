5c25bf1f271cc2e971c66a8636a4d8ec30ae52dd
KangJi <kjhmd9726@gmail.com>
2019-12-16 02:42:25 +0900

    Add validation check. Check empty input and password confirm input.

--- src/components/SignUpForm/SignUpForm.js
+++ src/components/SignUpForm/SignUpForm.js
@@ -29,10 +29,94 @@ const SignUpForm = props => {
     handleSubmit
   } = props;
 
+  const [identityValueIsEmpty, setIdentityValueIsEmpty] = useState(false);
+  const [passwordValueIsEmpty, setPasswordValueIsEmpty] = useState(false);
+  const [
+    passwordConfirmValueIsEmpty,
+    setPasswordConfirmValueIsEmpty
+  ] = useState(false);
+  const [passwordConfirmIsSame, setPasswordConfirmIsSame] = useState(true);
+  const [nameValueIsEmpty, setNameValueIsEmpty] = useState(false);
+  const [birthdayValueIsEmpty, setBirthdayValueIsEmpty] = useState(false);
+  const [nickNameValueIsEmpty, setNickNameValueIsEmpty] = useState(false);
+  const [genderValueIsEmpty, setGenderValueIsEmpty] = useState(false);
+  const [schoolValueIsEmpty, setSchoolValueIsEmpty] = useState(false);
+  const [emailValueIsEmpty, setEmailValueIsEmpty] = useState(false);
+  const [phoneNumberValueIsEmpty, setPhoneNumberValueIsEmpty] = useState(false);
+
+  const validationCheck = () => {
+    let valueIsEmpty = false;
+
+    if (identityValue === "") {
+      setIdentityValueIsEmpty(true);
+      valueIsEmpty = true;
+    } else setIdentityValueIsEmpty(false);
+
+    if (passwordValue === "") {
+      setPasswordValueIsEmpty(true);
+      valueIsEmpty = true;
+    } else setPasswordValueIsEmpty(false);
+
+    if (passwordConfirmValue === "") {
+      setPasswordConfirmValueIsEmpty(true);
+      valueIsEmpty = true;
+    } else setPasswordConfirmValueIsEmpty(false);
+
+    if (nameValue === "") {
+      setNameValueIsEmpty(true);
+      valueIsEmpty = true;
+    } else setNameValueIsEmpty(false);
+
+    if (birthdayValue === "") {
+      setBirthdayValueIsEmpty(true);
+      valueIsEmpty = true;
+    } else setBirthdayValueIsEmpty(false);
+
+    if (nickNameValue === "") {
+      setNickNameValueIsEmpty(true);
+      valueIsEmpty = true;
+    } else setNickNameValueIsEmpty(false);
+
+    if (genderValue === "") {
+      setGenderValueIsEmpty(true);
+      valueIsEmpty = true;
+    } else setGenderValueIsEmpty(false);
+
+    if (schoolValue === "") {
+      setSchoolValueIsEmpty(true);
+      valueIsEmpty = true;
+    } else setSchoolValueIsEmpty(false);
+
+    if (emailValue === "") {
+      setEmailValueIsEmpty(true);
+      valueIsEmpty = true;
+    } else setEmailValueIsEmpty(false);
+
+    if (phoneNumberValue === "") {
+      setPhoneNumberValueIsEmpty(true);
+      valueIsEmpty = true;
+    } else setPhoneNumberValueIsEmpty(false);
+
+    return !valueIsEmpty;
+  };
+
+  const passwordConfirmCheck = () => {
+    if (passwordValue === passwordConfirmValue) {
+      setPasswordConfirmIsSame(true);
+      return true;
+    }
+
+    setPasswordConfirmIsSame(false);
+    return false;
+  };
+
   const vaildationCheckAndHandleSubmit = event => {
     // Prevent browers refresh.
     event.preventDefault();
-    handleSubmit();
+
+    const emptyValidation = validationCheck();
+    const passwordConfirmValidation = passwordConfirmCheck();
+    if (emptyValidation && passwordConfirmValidation) handleSubmit();
   };
 
   return (
@@ -46,8 +130,8 @@ const SignUpForm = props => {
             className="signup-form-component__form__input-box-wrapper__id-input-box"
             label="ID"
             onChange={handleIdentityValue}
-            error={false}
-            helperText=" "
+            error={identityValueIsEmpty}
+            helperText={identityValueIsEmpty ? "Please fill ID" : " "}
             fullWidth
           />
         </div>
@@ -57,8 +141,8 @@ const SignUpForm = props => {
             label="Password"
             type="password"
             onChange={handlePasswordValue}
-            error={false}
-            helperText=" "
+            error={passwordValueIsEmpty}
+            helperText={passwordValueIsEmpty ? "Please fill password" : " "}
             fullWidth
           />
         </div>
@@ -68,8 +152,12 @@ const SignUpForm = props => {
             label="Password confirm"
             type="password"
             onChange={handlePasswordConfirmValue}
-            error={false}
-            helperText=" "
+            error={passwordConfirmValueIsEmpty || !passwordConfirmIsSame}
+            helperText={
+              passwordConfirmValueIsEmpty || !passwordConfirmIsSame
+                ? "Please check password confirm"
+                : " "
+            }
             fullWidth
           />
         </div>
@@ -78,8 +166,8 @@ const SignUpForm = props => {
             className="signup-form-component__form__input-box-wrapper__name-input-box"
             label="Name"
             onChange={handleNameValue}
-            error={false}
-            helperText=" "
+            error={nameValueIsEmpty}
+            helperText={nameValueIsEmpty ? "Please fill name" : " "}
             fullWidth
           />
         </div>
@@ -88,8 +176,8 @@ const SignUpForm = props => {
             className="signup-form-component__form__input-box-wrapper__birthday-input-box"
             label="Birthday"
             onChange={handleBirthdayValue}
-            error={false}
-            helperText=" "
+            error={birthdayValueIsEmpty}
+            helperText={birthdayValueIsEmpty ? "Please fill birthday" : " "}
             fullWidth
           />
         </div>
@@ -98,8 +186,8 @@ const SignUpForm = props => {
             className="signup-form-component__form__input-box-wrapper__nickname-input-box"
             label="Nickname"
             onChange={handleNickNameValue}
-            error={false}
-            helperText=" "
+            error={nickNameValueIsEmpty}
+            helperText={nickNameValueIsEmpty ? "Please fill nick name" : " "}
             fullWidth
           />
         </div>
@@ -108,8 +196,8 @@ const SignUpForm = props => {
             className="signup-form-component__form__input-box-wrapper__gender-input-box"
             label="Gender"
             onChange={handleGenderValue}
-            error={false}
-            helperText=" "
+            error={genderValueIsEmpty}
+            helperText={genderValueIsEmpty ? "Please fill gender" : " "}
             fullWidth
           />
         </div>
@@ -118,8 +206,8 @@ const SignUpForm = props => {
             className="signup-form-component__form__input-box-wrapper__school-input-box"
             label="School"
             onChange={handleSchoolValue}
-            error={false}
-            helperText=" "
+            error={schoolValueIsEmpty}
+            helperText={schoolValueIsEmpty ? "Please fill school" : " "}
             fullWidth
           />
         </div>
@@ -128,8 +216,8 @@ const SignUpForm = props => {
             className="signup-form-component__form__input-box-wrapper__email-input-box"
             label="Email"
             onChange={handleEmailValue}
-            error={false}
-            helperText=" "
+            error={emailValueIsEmpty}
+            helperText={emailValueIsEmpty ? "Please fill email" : " "}
             fullWidth
           />
         </div>
@@ -138,8 +226,10 @@ const SignUpForm = props => {
             className="signup-form-component__form__input-box-wrapper__phoneNumber-input-box"
             label="Phone number"
             onChange={handlePhoneNumberValue}
-            error={false}
-            helperText=" "
+            error={phoneNumberValueIsEmpty}
+            helperText={
+              phoneNumberValueIsEmpty ? "Please fill phone number" : " "
+            }
             fullWidth
           />
         </div>
