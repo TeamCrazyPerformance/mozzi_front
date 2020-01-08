a521ec52aa605e50ac7d03c59876c04c0c15a217
KangJi <kjhmd9726@gmail.com>
2020-01-09 00:27:12 +0900

    Add exam container component layout.

--- src/containers/Exam/Exams/Exams.js
+++ src/containers/Exam/Exams/Exams.js
@@ -1,5 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
+import ExamsTable from "../../../components/ExamsTable/ExamsTable.js";
+import LoadingSpinner from "../../../components/LoadingSpinner/LoadingSpinner";
+import Error from "../../../components/Error/Error";
 
-const Exams = () => <div>Exams</div>;
+const Exams = () => {
+  const [exams, setExams] = useState([
+    {
+      examId: 0,
+      authorId: 0,
+      year: "",
+      major: "",
+      garde: 0,
+      semeter: 0,
+      term: "",
+      name: "",
+      professor: ""
+    }
+  ]);
+  const [page, setPage] = useState(0);
+  const [count, setCount] = useState(0);
+  const [total, setTotal] = useState(0);
+  const [loadingState, setLoadingState] = useState(false);
+  const [error, setError] = useState(false);
+
+  return (
+    <div>
+      <div>Exams</div>
+      <LoadingSpinner loadingState={loadingState}>
+        {error ? (
+          <Error />
+        ) : (
+          <ExamsTable data={exams} page={page} count={count} total={total} />
+        )}
+      </LoadingSpinner>
+    </div>
+  );
+};
 
 export default Exams;
