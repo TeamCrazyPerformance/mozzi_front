f162ea4220a49e07cd27acec23780fe5cabb3296
KangJi <kjhmd9726@gmail.com>
2020-01-14 00:30:36 +0900

    Use fetch api to connect to server at login api call.

--- src/redux/auth/api.js
+++ src/redux/auth/api.js
@@ -6,26 +6,9 @@ export const postSignIn = ({ id, password }) => {
     .post("/user/login", { id, password })
     .then(response => checkExpirity(response.token));
 };
-// {
-//   return {
-//     success: true,
-//     role: "admin",
-//     token:
-//       "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiZXhwIjoxNjI3MjM5MDIyfQ.WSiharqFEJwXWM4F4_SFO-dwvVJHUXXCVsq7uJ0oFA4"
-//   };
-// };
 
-
-
-export const tokenRefresh = () => ({
-  success: true,
-  role: "admin",
-  token:
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiZXhwIjoxNjI3MjM5MDIyfQ.WSiharqFEJwXWM4F4_SFO-dwvVJHUXXCVsq7uJ0oFA4"
-});
-
-// {
-//   fetchHelper
-//     .post("/user/refresh")
-//     .then(response => checkExpirity(response.token));
-// };
+export const tokenRefresh = () => {
+  fetchHelper
+    .post("/user/refresh")
+    .then(response => checkExpirity(response.token));
+};
