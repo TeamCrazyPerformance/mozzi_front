a43893a4bc1783c5e3227c1a0dd68a1ecc5a03f0
KangJi <kjhmd9726@gmail.com>
2020-03-06 00:30:16 +0900

    Show edit button only to yourself.

--- src/containers/UserInformation/UserInformation/UserInformation.js
+++ src/containers/UserInformation/UserInformation/UserInformation.js
@@ -1,16 +1,16 @@
 import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
+import { connect } from "react-redux";
 import Button from "@material-ui/core/Button";
 import UserInformationList from "../../../components/UserInformationList/UserInformationList";
 import LoadingSpinner from "../../../components/LoadingSpinner/LoadingSpinner";
 import Error from "../../../components/Error/Error";
 import * as userInformationApi from "./UserInformationApi";
 
-const UserInformation = ({
-  match: {
-    params: { userId }
-  }
-}) => {
+const UserInformation = props => {
+  const { myId, match } = props;
+  const { userId } = match.params;
   const [userInformation, setUserInformation] = useState({
     id: "",
     name: "",
@@ -35,7 +35,7 @@ const UserInformation = ({
 
   const setLoadingStateAndErrorWhenApiCallSuccess = responseUserInformation => {
     setUserInformation(responseUserInformation);
-    if (responseUserInformation.id === userId) setSelfIdentification(true);
+    if (responseUserInformation.id === myId) setSelfIdentification(true);
     setLoadingState(false);
     setError(false);
   };
@@ -85,6 +85,7 @@ const UserInformation = ({
 };
 
 UserInformation.propTypes = {
+  myId: PropTypes.string.isRequired,
   match: PropTypes.shape({
     params: PropTypes.shape({
       userId: PropTypes.string.isRequired
@@ -92,4 +93,9 @@ UserInformation.propTypes = {
   }).isRequired
 };
 
-export default UserInformation;
+const mapStateToProps = state => {
+  const { auth } = state;
+  return { myId: auth.userId };
+};
+
+export default connect(mapStateToProps)(UserInformation);
