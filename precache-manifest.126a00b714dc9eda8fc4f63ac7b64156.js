9231bd0f06d59826812ddf1330153d386f7aec12
KangJiJi <kjhmd9726@gmail.com>
2020-08-09 03:31:58 +0900

    Updates

---
+++ precache-manifest.126a00b714dc9eda8fc4f63ac7b64156.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c9f92dc2c2b9d36ab897ee777ac3ce6",
+    "url": "/mozzi_front/index.html"
+  },
+  {
+    "revision": "b70ceec5f2e720bf2fc4",
+    "url": "/mozzi_front/static/css/main.26d49fbf.chunk.css"
+  },
+  {
+    "revision": "857f25e97470a7c0fde2",
+    "url": "/mozzi_front/static/js/2.c0b770c7.chunk.js"
+  },
+  {
+    "revision": "b70ceec5f2e720bf2fc4",
+    "url": "/mozzi_front/static/js/main.e53d6297.chunk.js"
+  },
+  {
+    "revision": "55866a3fefcb2e4940e3",
+    "url": "/mozzi_front/static/js/runtime-main.afb1a5e5.js"
+  },
+  {
+    "revision": "6cfe430d79fd169e809a81c9e3bb880c",
+    "url": "/mozzi_front/static/media/logo.6cfe430d.png"
+  }
+]);
